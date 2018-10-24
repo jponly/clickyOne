7f0a3750e325f5e5e562067466626ac37c5aa7db
jponly <jpangeli514@gmail.com>
2018-10-23 22:01:57 -0700

    Updates

---
+++ precache-manifest.c160955d862b27366bd378faadb83080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a993d4940354b91643a",
+    "url": "/clickyOne/static/js/runtime~main.1a993d49.js"
+  },
+  {
+    "revision": "2e7c6cf24f3a422b8e41",
+    "url": "/clickyOne/static/js/main.2e7c6cf2.chunk.js"
+  },
+  {
+    "revision": "6fea6cd249d4f943684f",
+    "url": "/clickyOne/static/js/1.6fea6cd2.chunk.js"
+  },
+  {
+    "revision": "2e7c6cf24f3a422b8e41",
+    "url": "/clickyOne/static/css/main.746bd50b.chunk.css"
+  },
+  {
+    "revision": "ca9b674786748ad52f6d159b2d0365d6",
+    "url": "/clickyOne/index.html"
+  }
+];
